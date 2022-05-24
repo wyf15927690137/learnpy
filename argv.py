f5e668f1ceff4145a74b2f6be6e509be234952c0
wyf15927690137 <weiyanfei@sjtu.edu.cn>
2022-05-24 10:35:58 +0800

    local

--- argv.py
+++ argv.py
@@ -1,5 +1,16 @@
+import sys
+
 print('this is a test file')
 print('this is the second line from cloud')
 print('this is the second line from cloud1')
 print('this is the third line from Local')
 print('this is the third line from Local1')
+s1 = sys.argv[1]
+s2 = sys.argv[2]
+s3 = sys.argv[3]
+print(s1)
+print(s2)
+print(type(s3))  # view the datatype
+s4 = int(s3)
+print(type(s4))
+print(int(s2) * int(s3))
